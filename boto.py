ea28794f561072201db75713ed056a9b96e0f56b
Tal_Kleiner <talkl120@gmail.com>
2018-12-13 08:19:31 +0200

    added swear words filter

--- boto.py
+++ boto.py
@@ -1,8 +1,12 @@
 """
 This is the template server side for ChatBot
 """
-from bottle import route, run, template, static_file, request
 import json
+
+from bottle import route, run, template, static_file, request
+
+with open("./swear_words/swear_words.txt") as f:
+    swear_words = sorted(word.strip(" ") for line in f for word in line.split(','))
 
 
 @route('/', method='GET')
@@ -10,10 +14,26 @@ def index():
     return template("chatbot.html")
 
 
+def cursing_exists(user_text):
+    words = user_text.split(" ")
+    if any(word in swear_words for word in words):
+        return "Hey! your'e not allowed to swear", 'heartbroke'
+    else:
+        return False
+
+
+def analyze_user_message(msg):
+    response_message, animation = "Sorry, seems like i don't have an appropriate response in my algorithm", 'crying'
+    if cursing_exists(msg):
+        response_message, animation = cursing_exists(msg)
+    return response_message, animation
+
+
 @route("/chat", method='POST')
 def chat():
     user_message = request.POST.get('msg')
-    return json.dumps({"animation": "inlove", "msg": user_message})
+    response_message, animation = analyze_user_message(user_message)
+    return json.dumps({"animation": animation, "msg": response_message})
 
 
 @route("/test", method='POST')
@@ -39,6 +59,7 @@ def images(filename):
 
 def main():
     run(host='localhost', port=7000)
+
 
 if __name__ == '__main__':
     main()
